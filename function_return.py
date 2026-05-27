f1d78b8fcfa0860eedcc86de4ed5c403370e706e
Santhosh Ramachandra <sannunice4u@gmail.com>
2026-05-27 21:24:37 +0530

    Implement add, multiply, and divide functions

---
+++ function_return.py
@@ -0,0 +1,10 @@
+def add_numbers(a, b):
+    add_numbers = a + b
+    multiply_numbers = a * b
+    divide_numbers = a / b
+    return add_numbers, multiply_numbers, divide_numbers
+
+result1, result2, result3 = add_numbers(5, 10)
+print(result1)
+print(result2)
+print(result3)
