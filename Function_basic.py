9225637fd9d3a14b9fcc6ad03720f7f6e37dda94
Santhosh Ramachandra <sannunice4u@gmail.com>
2026-05-27 21:29:37 +0530

    Add function to sum two numbers and call it

---
+++ Function_basic.py
@@ -0,0 +1,5 @@
+def add_numbers(x,y): # define a function called add_numbers that takes two parameters, x and y
+    print(x+y)
+add_numbers(5,10) #calling the function add_numbers 
+add_numbers(20,30)
+add_numbers(100,200)
